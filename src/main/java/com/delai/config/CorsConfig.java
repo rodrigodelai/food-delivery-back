d09ecef05499b2889f3c04f8f4845bec63bdc115
Rodrigo Delai <rodrigo-delai@hotmail.com>
2024-06-30 19:48:32 -0300

    feat: create prod profile and configure deploy

---
+++ src/main/java/com/delai/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.delai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Profile("prod")
+public class CorsConfig {
+
+    @Bean
+    CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://delai-food-delivery.netlify.app");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
